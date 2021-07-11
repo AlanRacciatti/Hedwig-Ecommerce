0c410c72b022935f9e20fdf4fb2bcf85d4e7012b
Alex Racciatti <racciattialex@gmail.com>
2021-07-11 20:20:28 -0300

    Procfile + parámetros en el app.listen para poder deployear la pagina en Heroku

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, './public')));
 app.use(express.static(path.join(__dirname, './views')));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Corriendo puerto 3000');
 });
 
