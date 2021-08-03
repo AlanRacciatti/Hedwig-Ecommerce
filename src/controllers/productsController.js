1f9ae03cb9a08864ae9589a0fab676c31857e94d
BlueRios <mazulrios@gmail.com>
2021-08-03 20:24:50 -0300

    add metodo creacion de producto

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,3 +1,8 @@
+const fs = require("fs");
+const path = require("path");
+const productsFilePath = path.join(__dirname, '../data/productsDB.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 const controladorProductos = {
     index: (req, res) => {
         res.render("./products/index");
@@ -12,7 +17,35 @@ const controladorProductos = {
     },
 
     agregarProducto: (req,res) => {
-        res.render("./products/agregarProducto");
+        res.render("./products/create");
+    },
+
+    store: (req,res) => {
+        let idNuevo= 0
+        for (let p of products){
+            if (idNuevo < p.id){
+                idNuevo = p.id
+            }
+        }
+        idNuevo++
+        let nombreImagen = req.file.filename
+        let productoNuevo = {
+            id: idNuevo,
+            title: req.body.title,
+            description: req.body.description,
+            price: req.body.price,
+            offer: req.body.offer,
+            rating: req.body.rating,
+            gender: req.body.gender,
+            author: req.body.author,
+            image: nombreImagen,
+            pagsLength: req.body.pagsLength,
+            editorial:req.body.editorial,
+
+        }
+        products.push(productoNuevo)
+        fs.writeFileSync(productsFilePath,JSON.stringify(products,null," "))
+        res.redirect("/")
     },
 
     editarProducto: (req,res) => {
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,7 +1,25 @@
-const productsController = require("./../controllers/productsController");
-
+// ** Require's **
 const express = require("express");
 const router = express.Router();
+const path = require('path');
+
+const multer = require('multer');
+
+// ** Controller require **
+const productsController = require("./../controllers/productsController");
+
+// ** Multer config *
+const configuracionImagen = multer.diskStorage({
+    destination: function(req, file, cb) {       // request, archivo y callback que almacena archivo en destino
+     cb(null, path.join(__dirname,'../../public/img/products'));    // Ruta donde almacenamos el archivo
+    },
+    filename: function(req, file, cb) {          // request, archivo y callback que almacena archivo en destino
+     let imageName =  Date.now() + file.originalname ;   // milisegundos y extensión de archivo original
+     cb(null, imageName);
+    }
+});
+
+const uploadFile = multer({ storage: configuracionImagen });
 
 router.get("/", productsController.index);
 
@@ -9,7 +27,8 @@ router.get("/detalles", productsController.detalles);
 
 router.get("/carrito", productsController.carrito);
 
-router.get("/agregarProducto", productsController.agregarProducto);
+router.get("/create", productsController.agregarProducto);
+router.post("/create", uploadFile.single("image"), productsController.store)
 
 router.get("/editarProducto", productsController.editarProducto);
 
