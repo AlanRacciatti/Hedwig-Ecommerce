3d135aa16e5ecccd88747757187692e93f01c973
Alex Racciatti <racciattialex@gmail.com>
2021-09-13 22:59:47 -0300

    add configuración de la base de datos productiva y de testeo de alwaysData; delete vinculos a /register

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -8,17 +8,17 @@ module.exports = {
     "port": 3306
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": "hedwig",
+    "password": "alwaysdata2605",
+    "database": "hedwig_testeo",
+    "host": "mysql-hedwig.alwaysdata.net",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "hedwig",
+    "password": "alwaysdata2605",
+    "database": "hedwig_db",
+    "host": "mysql-hedwig.alwaysdata.net",
     "dialect": "mysql"
   }
 }
