0e4df0d3bbc923cb746dd0a7d6d9eeca9dcef049
Alex Racciatti <racciattialex@gmail.com>
2021-10-09 20:44:16 -0300

    add validacion front de creación de producto

--- public/js/validations/frontValidationLogin.js
+++ public/js/validations/frontValidationLogin.js
@@ -1,41 +1,41 @@
-console.log("hola")
-
 const form = document.getElementById("form-login");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const emailValidation = document.getElementById("email-validation")
 const passwordValidation = document.getElementById("password-validation")
 
-form.addEventListener("submit", (e) => {
+window.addEventListener('load', function(){
+    form.addEventListener("submit", (e) => {
 
-    let formIsOk = true
+        let formIsOk = true
 
-    if (email.value == "" || email.value == null) {
+        if (email.value == "" || email.value == null) {
 
-        emailValidation.classList.remove("d-none")
-        email.classList.add("border")
-        email.classList.add("border-danger")
+            emailValidation.classList.remove("d-none")
+            email.classList.add("border")
+            email.classList.add("border-danger")
 
-        formIsOk = false
-    } else {
-        emailValidation.classList.add("d-none")
-        
-        email.classList.remove("border")
-        email.classList.remove("border-danger")
-    }
+            formIsOk = false
+        } else {
+            emailValidation.classList.add("d-none")
+            
+            email.classList.remove("border")
+            email.classList.remove("border-danger")
+        }
 
-    if (password.value == "" || password.value == null) {
-        passwordValidation.classList.remove("d-none");
-        password.classList.add("border")
-        password.classList.add("border-danger")
-        formIsOk = false
-    } else {
-        passwordValidation.classList.add("d-none")
-        password.classList.remove("border")
-        password.classList.remove("border-danger")
-    }
+        if (password.value == "" || password.value == null) {
+            passwordValidation.classList.remove("d-none");
+            password.classList.add("border")
+            password.classList.add("border-danger")
+            formIsOk = false
+        } else {
+            passwordValidation.classList.add("d-none")
+            password.classList.remove("border")
+            password.classList.remove("border-danger")
+        }
 
-    if (!formIsOk) {
-        e.preventDefault()
-    }
-})
+        if (!formIsOk) {
+            e.preventDefault()
+        }
+    })
+})   
