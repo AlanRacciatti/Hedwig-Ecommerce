d902c8fac5d1c7b6188479d0487c4e82820de4cd
AlanRacciatti <alanracciatti1220@gmail.com>
2021-10-11 20:50:28 -0300

    add búsqueda de productos

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,4 +1,5 @@
 const db = require('../database/models');
+const { Op } = require('sequelize');
 
 const controladorMain = {
     index: (req, res) => {
@@ -8,6 +9,27 @@ const controladorMain = {
         })
       
     },
+    search: (req, res) => {
+
+        let { search } = req.query
+
+            db.libros.findAll({
+
+                where: {
+                    [Op.or]: {
+                        titulo: {
+                            [Op.like]: `%${search}%`
+                        },
+                        descripcion: {
+                            [Op.like]: `%${search}%`
+                        }   
+                    }
+                }
+                
+            })
+            .then(resultado => res.render("./products/index", {data: {products: resultado, session: req.session}}))
+
+    }
 }
 
 module.exports = controladorMain;
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,8 +1,8 @@
 const mainControllers = require("../controllers/mainController");
-
 const express= require("express");
-
 const router = express.Router() 
+
 router.get("/", mainControllers.index);
+router.get("/search", mainControllers.search);
 
 module.exports = router
